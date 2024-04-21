13165822aa241981864a14f1eb936df3cd9f73b7
Xen0n-n <nsoft13@gmail.com>
2024-04-21 17:42:17 +0300

    shop/ modified to OOP structure

--- shop/source/scripts/main.js
+++ shop/source/scripts/main.js
@@ -1,23 +1,44 @@
-const products = [
-    { id: 1, title: 'Notebook', price: 2000 },
-    { id: 2, title: 'Mouse', price: 20 },
-    { id: 3, title: 'Keyboard', price: 200 },
-    { id: 4, title: 'Gamepad', price: 50 },
-];
-//Функция для формирования верстки каждого товара
-const renderProduct = (product) => {
-    return `<div class="product-item">
-                <img src="https://via.placeholder.com/200x200/258DC8/E0F6FD" alt="">
-                <h3>${product.title}</h3>
-                <p>${product.price} ₽</p>
+class ProductsList {
+  constructor(container = ".products") {
+    this.container = container;
+    this.goods = [];
+    this._fetchProducts();
+  }
+
+  _fetchProducts() {
+    this.goods = [
+      { id: 1, title: "Notebook", price: 2000 },
+      { id: 2, title: "Mouse", price: 20 },
+      { id: 3, title: "Keyboard", price: 200 },
+      { id: 4, title: "Gamepad", price: 50 },
+    ];
+  }
+  render() {
+    const block = document.querySelector(this.container);
+    for (let product of this.goods) {
+      const productObj = new ProductItem(product);
+      block.insertAdjacentHTML("beforeend", productObj.render());
+    }
+  }
+}
+
+class ProductItem {
+  constructor(product, img = "https://via.placeholder.com/200x150") {
+    this.title = product.title;
+    this.price = product.price;
+    this.id = product.id;
+    this.img = img;
+  }
+
+  render() {
+    return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="">
+                <h3>${this.title}</h3>
+                <p>${this.price} ₽</p>
                 <button class="buy-btn">Купить</button>
             </div>`
-};
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item));
-    console.log(productsList);
+  }
+}
 
-    document.querySelector('.products').innerHTML = productsList.join('');
-};
-
-renderPage(products);
+let list = new ProductsList();
+list.render();
---
+++ shop/source/scripts/main_non_actual.js
@@ -0,0 +1,23 @@
+const products = [
+    { id: 1, title: 'Notebook', price: 2000 },
+    { id: 2, title: 'Mouse', price: 20 },
+    { id: 3, title: 'Keyboard', price: 200 },
+    { id: 4, title: 'Gamepad', price: 50 },
+];
+//Функция для формирования верстки каждого товара
+const renderProduct = (product) => {
+    return `<div class="product-item">
+                <img src="https://via.placeholder.com/200x150" alt="">
+                <h3>${product.title}</h3>
+                <p>${product.price} ₽</p>
+                <button class="buy-btn">Купить</button>
+            </div>`
+};
+const renderPage = list => {
+    const productsList = list.map(item => renderProduct(item));
+    console.log(productsList);
+
+    document.querySelector('.products').innerHTML = productsList.join('');
+};
+
+renderPage(products);
