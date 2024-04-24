83be50f3d6422609ecf74043c0921b2e11a013dc
Xen0n-n <nsoft13@gmail.com>
2024-04-24 13:18:15 +0300

    Finished icecream factory

--- ice-cream-factory/icecream-factory.js
+++ ice-cream-factory/icecream-factory.js
@@ -10,18 +10,30 @@ class Icecream {
         this.size = new Param(this._select(size));
         this.type = new Param(this._select(type));
         this.stuff = new Param(this._select(stuff));
+        this.adds = this._getAdds(adds);
+    }
+
+    _getAdds(name) {
+        let result = [];
+        this._selectAll(name).forEach(el => result.push(new Param(el)));
+        return result
     }
 
     _select(name) {
         return document.querySelector(`input[name=${name}]:checked`);
     }
 
-    _sumPrice(){
+    _selectAll(name) {
+        return [...document.querySelectorAll(`input[name=${name}]:checked`)];
+    }
+
+    _sumPrice() {
         let result = this.size.price + this.type.price + this.stuff.price;
+        this.adds.forEach(el => result += el.price)
         return result
     }
 
-    showSum(price){
+    showSum(price) {
         document.querySelector(price).textContent = this._sumPrice();
     }
 }
