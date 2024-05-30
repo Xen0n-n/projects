cd563951a1f3bf325e732b5d809a34eb9aed5a40
Xen0n-n <nsoft13@gmail.com>
2024-05-30 18:46:23 +0300

    added submit button

--- feedback_form/source/scripts/validation.js
+++ feedback_form/source/scripts/validation.js
@@ -1,11 +1,14 @@
 function email_validation(input){
+
     return input.match(/^[\w\d]+\@[\w]+\.[\w]{2,5}$/i)
 }
 
 function phone_validation(input){
+
     return input.match(/\+7\(\d{3}\)\d{3}-\d{2}-\d{2}/)
 }
 
-console.log(email_validation('nsoft13@gmail.com'))
-console.log(phone_validation('+7(977)966-83-52'))
-
+function validate(){
+    console.log(email_validation('nsoft13@gmail.com'))
+    console.log(phone_validation('+7(977)966-83-52'))
+}
