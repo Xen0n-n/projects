3c346644bd19eb196e9621832575870751bbad1d
Xen0n-n <nsoft13@gmail.com>
2024-05-30 18:25:52 +0300

    added phone validation

--- feedback_form/source/scripts/validation.js
+++ feedback_form/source/scripts/validation.js
@@ -1,5 +1,11 @@
 function email_validation(input){
     return input.match(/^[\w\d]+\@[\w]+\.[\w]{2,5}$/i)
 }
+
+function phone_validation(input){
+    return input.match(/\+7\(\d{3}\)\d{3}-\d{2}-\d{2}/)
+}
+
 console.log(email_validation('nsoft13@gmail.com'))
+console.log(phone_validation('+7(977)966-83-52'))
 
