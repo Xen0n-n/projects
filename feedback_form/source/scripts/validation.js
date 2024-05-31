093955ba23891237520edc7913e30ea1886f6c74
Xen0n-n <nsoft13@gmail.com>
2024-05-31 13:59:06 +0300

    added submit and error code

--- feedback_form/source/scripts/validation.js
+++ feedback_form/source/scripts/validation.js
@@ -1,18 +1,16 @@
 function email_validation(input){
-
     return input.match(/^[\w\d]+\@[\w]+\.[\w]{2,5}$/i)
 }
 
 function phone_validation(input){
-
     return input.match(/\+7\(\d{3}\)\d{3}-\d{2}-\d{2}/)
 }
 
-function validate(){
-    const formData = new FormData(document.getElementById('feedback-form'));
+function validate(formData){
+    
     const emailValCode = email_validation(formData.get('email'))
     const phoneValCode = phone_validation(formData.get('phone'))
-    
+
     if ((!emailValCode) || (!phoneValCode)){
         if (emailValCode === null) {
             document.getElementById('email').style.borderColor = 'red';
@@ -24,14 +22,27 @@ function validate(){
         } else {
             document.getElementById('phone').style.borderColor = 'green';
         }
-        
+        return false;
     } else {
         document.getElementById('email').style.borderColor = 'green';
         document.getElementById('phone').style.borderColor = 'green';
+        return true;
     }
-    
+}
 
-    console.log(email_validation(formData.get('email')))
-    console.log(phone_validation(formData.get('phone')))
+function check_and_submit(event){
+    event.preventDefault();
+    const formData = new FormData(document.getElementById('feedback-form'));
+    
+    console.log(email_validation(formData.get('email')));
+    console.log(phone_validation(formData.get('phone')));
+
+    if (validate(formData)){
+        alert('success');
+        return true;
+    } else {
+        alert('Error: Check your input');
+        return fasle;
+    }
 }
 
